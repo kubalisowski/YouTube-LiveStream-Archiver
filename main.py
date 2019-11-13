36c8554b260e26c628c28a4666a5b31d250613ba
Kuba <kubalisowski94@gmail.com>
2019-11-13 20:34:17 +0100

    Check if Stream is live + start recording
    Live Stream checking - if is live recording is started;
    LiveStream.startRecording() returns list of live processes (with name variable equal to key from Config.channels)

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-import multiprocessing
-from stream import Record, LiveStream
+from stream import LiveStream
 
-def main(liveStreams):
+stream = LiveStream()
+rec = stream.startRecording()
+
+### If .poll() returns None process is live
+for r in rec:
+    print(r.poll())
 
 
-    pass
 
 
-
-
-liveProcess = []
-
-if __name__ == '__main__':
-    proc = multiprocessing.Process(target=main, args=())
-    for l in liveProcess:
-        if l.is_alive() == False:
-               liveProcess.remove(l)
-        else:
-            if proc.name != l.name:
-                ### Start process and add to live streams list
-                liveProcess.append(proc.start())
-            else:
-                pass
 
 
 
--- stream.py
+++ stream.py
@@ -4,21 +4,22 @@ import json
 from config import Config
 
 class LiveStream(Config):
-    ### Returns dict of live streams jasons + names from config
-    def isLive(self):
-        liveStreams = []
-        for key, value in self.channels.items():
-            response = requests.get(value, self.headers).text
-            jason = json.loads(response)
-
-            if jason['pageInfo']['totalResults'] == 1:
-                liveStreams[key] = jason
-            return liveStreams
+    liveStreams = {}
+    for key, value in Config.channels.items():
+        jason = json.loads(requests.get(value, Config.headers).text)
+        if jason['pageInfo']['totalResults'] == 1:
+            liveStreams.update({key : jason})
 
     def startRecording(self):
-        videoUrl = self.baseUrl + self.liveStream['items']['id']['videoId']
-        proc = subprocess.Popen(['youtube-dl', videoUrl], shell=False, name=self.name)
-        return proc
+        processes = []
+        for key, value in self.liveStreams.items():
+            videoUrl = self.baseUrl + value['items'][0]['id']['videoId']
+            proc = subprocess.Popen(['youtube-dl', videoUrl], shell=False)
+            ### Set name to each process
+            proc.name = key
+            processes.append(proc)
+        return processes
+
 
 
 
