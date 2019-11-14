7dc695eea6a8ca6284ab83cc70dfade96d2deaef
Kuba <kubalisowski94@gmail.com>
2019-11-14 01:04:35 +0100

    Content warning fix
    If Content warning -- check if YouTube account can access stream via browser

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ class Config:
     auth2 = 'AIzaSyBFB6euD6FKO6OwixkSENe4FJtejE1ahXw'
     auth = 'AIzaSyBag5dZLIOwtU-UO8Cr8iXK4Gv0ZV8VmLU'
 
-    channels = {'Telewizja wPolsce' : 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPiu4CZlknkTworskK79CPg&type=video&eventType=live&key=' + auth,
-                'Rzeczpospolita TV' : 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCpchzx2u5Ab8YASeJsR1WIw&type=video&eventType=live&key=' + auth,
-                }
+    channels = {'rafatus': 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCeAun7EBsIyk-1jYGx2NLeg&type=video&eventType=live&key=' + auth, 'Telewizja wPolsce' : 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPiu4CZlknkTworskK79CPg&type=video&eventType=live&key=' + auth, 'Rzeczpospolita TV' : 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCpchzx2u5Ab8YASeJsR1WIw&type=video&eventType=live&key=' + auth,}
 
+# Content warning -- check if YouTube account can access stream via browser
 # 'rafatus': 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCeAun7EBsIyk-1jYGx2NLeg&type=video&eventType=live&key=' + auth
