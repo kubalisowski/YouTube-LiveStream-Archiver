f98b7579c25ac3bdd979faadb79031a9996d7fca
Kuba <kubalisowski94@gmail.com>
2019-11-17 22:34:38 +0100

    Process Manager + schedule
    Process manager:
    - Kill inactive procs
    - Compare live streams list to current recording vids

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from stream import LiveStream, Record
+from this_stream import LiveStream, Record, ProcManager
+import schedule, time
 
-procs = []
+processes = []
 
 def main(processes):
-    streams = LiveStream()
-    for key, value in streams.liveStreams.items():
-        rec = Record(key, value)
-        processes.append(rec.startRecording())
-    return processes
+    liveStreams = LiveStream() # Get list of streams live
+    manager = ProcManager()
+    newStreams = manager.compareProcStream(manager.inactiveKiller(processes), liveStreams) # Dict of live, but not recording now streams
+    if len(newStreams) > 0:
+        rec = Record()
+        for key, value in newStreams:
+            processes.append(rec.startRecording(key, value))
+        return processes
+    else: # Zero new streams
+        return list()
 
-m = main(procs)
-print(m)
+def test():
+    print('2nd func')
 
-# while True:
-#     for p in procs:
-#         if p.poll() == None:
-#             print(p.name)
+schedule.every(5).minutes.do(main(processes))
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+
 
 
 
--- stream.py
+++ stream.py
@@ -1,26 +1,44 @@
 import subprocess
 import requests
 import json
-from config import Config
+from test_config import Config
+
+class ProcManager:
+    def inactiveKiller(self, processes): #  # Delete inactive subprocess object (subrocess_object.poll() != None)
+        for p in processes:
+            if p.poll() != None:
+                processes.remove(p)
+        return processes
+
+    def compareProcStream(self, processes, liveStreams): # Def returns dict of stremas to start be recording
+        if processes == 0:
+            return liveStreams # No processes running thus all live streams can be recorder
+        else:
+            if liveStreams > 0: # 1 on more live streams
+               for p in processes:
+                   keys = tuple(liveStreams.keys())
+                   for key in keys:
+                       if p.name == key:
+                           del liveStreams[key]
+                newStreams = liveStreams # # Dict of live, but not recording now streams (just naming change)
+                return newStreams
+            else:
+                return dict() # Zero live streams returns empty dict object
+
+class Record():
+    def startRecording(self, channelName, videoUrl):
+        proc = subprocess.Popen(['youtube-dl', videoUrl], shell=False)
+        proc.name = channelName
+        return proc
 
 class LiveStream(Config):
-    ### liveStreams variable is dict {channel_name : livestream_url,}
-    liveStreams = {}
+    liveStreams = {} ### liveStreams variable is dict {channel_name : livestream_url,}
     for key, value in Config.channels.items():
         jason = json.loads(requests.get(value, Config.headers).text)
         if jason['pageInfo']['totalResults'] == 1:
             liveStreams.update({key : Config.baseUrl + jason['items'][0]['id']['videoId']})
 
 
-class Record():
-    def __init__(self, channelName, videoUrl):
-        self.channelName = channelName
-        self.videoUrl = videoUrl
-
-    def startRecording(self):
-        proc = subprocess.Popen(['youtube-dl', self.videoUrl], shell=False)
-        proc.name = self.channelName
-        return proc
 
 
 
