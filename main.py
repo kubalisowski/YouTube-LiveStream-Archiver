1ea9efa38641b4825f16fdea11008bf901075e2e
Kuba <kubalisowski94@gmail.com>
2019-11-19 00:06:38 +0100

    Working ! __alpha__
    App downloading streams as assumped:
    - No-live streams are skipped
    - Live streams are saving in the script location

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-#################################################################################
-########## https://developers.google.com/youtube/v3/getting-started #############
-########## LAST DONE: UNKNOWN -- DAY LIMIT FOR AUTH KEY EXPIRED #################
-#################################################################################
 from stream import LiveStream, Record, ProcManager, Main
 import schedule, time
 
-
+global procs
 
 def main(procs):
     mainObj = Main()
     actualProcesses = mainObj.exec(procs)
     return actualProcesses
 
-procs = []
+procs = list()
 mn = main(procs)
 # print(mn)
 
 # schedule.every(0).minutes.do(main)
 #
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-#     processes = main()
-#     if len(processes) > 0:
-#         print(processes)
-
-
+while True:
+    # schedule.run_pending()
+    processes = main(procs)
+    for p in processes:
+        print(p.name)
+    time.sleep(1)
 
 
 
--- stream.py
+++ stream.py
@@ -13,8 +13,8 @@ class ProcManager:
         else:
             processes = [] # Prevent NoneType
             return processes
-            
-            
+
+
     def compareProcStream(self, processes, liveStreams):  # Def returns dict of stremas to start be recording
         if len(processes) == 0:
             return liveStreams  # No processes running thus all live streams can be recorder
@@ -25,8 +25,7 @@ class ProcManager:
                     for key in keys:
                         if p.name == key:
                             del liveStreams[key]
-                            newStreams = liveStreams  # # Dict of live, but not recording now streams (just naming change)
-                    return newStreams
+                return liveStreams   # Dict of live, but not recording now streams
             else:
                 return dict()  # Zero live streams returns empty dict object
 
@@ -47,20 +46,14 @@ class LiveStream(Config):
 
 
 class Main(ProcManager, Record, LiveStream):
-    # def __init__(self, processes): # for scheduling main instead of main() must be called
-    #     self.processes = processes
-
     def exec(self, processes):
         liveStreams = LiveStream()  # Get list of streams live
         manager = ProcManager()
         manager.inactiveKiller(processes)
+
         newStreams = manager.compareProcStream(manager.inactiveKiller(processes), liveStreams.liveStreams) # Dict of live, but not recording now streams
         lenghtStreams = len(newStreams)
-        # if len(newStreams) != int():
-        #     lenghtStreams = 0 # NoneType fix
-        # else:
-        #     lenghtStreams = len(newStreams)  # Create variable before value is assigned
-        #
+
         if lenghtStreams > 0:
             for key, value in newStreams.items():
                 rec = Record()
@@ -68,6 +61,6 @@ class Main(ProcManager, Record, LiveStream):
             return processes
         else: # Zero new streams
             return list()
-            
-            
+
+
             
